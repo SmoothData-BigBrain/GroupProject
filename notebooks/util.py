f352493e2473eb9bcedd4a86caceae5838802ecd
Mihir Patel <mpjv12343@gmail.com>
2025-05-10 15:28:10 -0700

    Testing (#25)
    * updated util for directory checking
    
    * updated notebook to fetch data dynamically
    
    * added new ignore, deleted old data path, updated code to read correct import
    
    * add ignore to temp file folders, update notebook to semi-final code
    
    * updated
    
    * resetting output for notebook
    
    * Nams Update (#24)
    
    Co-authored-by: Nam <nap011@ucsd.edu>
    
    * ignored utility folders
    
    * Updated notebook to be organized, changed some code for speed
    
    * updated README.md to remove col descriptions and name
    
    * deleted extra random files
    
    * minor change for block organization
    
    * Added Ahyos graphs
    
    * added virtual env ignore
    
    * deleted virtual_docs
    
    * ran Ahyo's code so plots show in output
    
    * Update GroupProject.ipynb
    
    Added descriptions to my plots and added code to change the airline codes to their actual names
    
    * Added discussion
    
    * Fixed origin delay code
    
    * Update GroupProject.ipynb
    
    readded descriptions and code for airline names
    
    ---------
    
    Co-authored-by: Nam <nap011@ucsd.edu>
    Co-authored-by: Ahyo2 <afalick@ucsd.edu>
    Co-authored-by: hgarma <hgarma@ucsd.edu>
    Co-authored-by: Rita Zaarour <ritazaarour9@gmail.com>

--- notebooks/util.py
+++ notebooks/util.py
@@ -26,7 +26,7 @@ def fetch_data(file_id, download_path, extract_to):
 
     print(f"File extracted to: {extract_to}")
 
-def check_and_fetch_data(data_dir):
+def check_and_fetch_data(file_id, data_dir):
     """
     Checks if there are any CSV files in the specified directory.
     If not, it triggers the fetch_data function to download and extract the data.
@@ -34,12 +34,15 @@ def check_and_fetch_data(data_dir):
     Parameters:
     - data_dir: str, directory to check for CSV files and download the data
     """
+
+    # # Ensure the extraction folder exists
+    os.makedirs(data_dir+'archive/raw/', exist_ok=True) # maybe remove the +...
+
     # Define paths
     download_path = os.path.join(data_dir, 'airlines.zip')  # Path to save the downloaded zip file
-    file_id = '1tch7xbFIgBtXKXa16E4QCpVKedUExfO3'  # My File ID for airlines.zip on GDrive 
 
     # Check if any .CSV files exist in the directory
-    csv_files = [f for f in os.listdir(data_dir+'/archive/raw') if f.endswith('.csv')]
+    csv_files = [f for f in os.listdir(data_dir+'archive/raw/') if f.endswith('.csv')]
 
     if not csv_files:
         print(f"No CSV files found in {data_dir}. Fetching data...")
